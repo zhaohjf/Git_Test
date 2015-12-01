7cbeb5568ac22ae52a0c9a10309a7f90ca84f84a
zhaohjf <zhaohj0315@163.com>
2015-12-01 20:07:02 +0800

    lucene学习资料
    Change-Id: Id23e5647404b8ef5fc0e9b31a3d4297a058adb98
    Signed-off-by: zhaohjf <zhaohj0315@163.com>

---
+++ src/com/cn/zhaohjg/git/test/record.java
@@ -0,0 +1,10 @@
+package com.cn.zhaohjg.git.test;
+
+/**
+ * http://blog.csdn.net/isea533/article/details/48791309
+ * http://www.ibm.com/developerworks/cn/java/j-lo-lucene1/
+ *
+ */
+public class record {
+
+}
