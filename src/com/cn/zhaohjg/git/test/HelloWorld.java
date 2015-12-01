d769e9f889eafe800badc2b87c78bcda77bfb2d1
zhaohjf <zhaohj0315@163.com>
2015-12-01 19:48:24 +0800

    5
    Change-Id: Icd00837346f10ec1f0996f6c9f271bee56417054
    Signed-off-by: zhaohjf <zhaohj0315@163.com>

--- src/com/cn/zhaohjg/git/test/HelloWorld.java
+++ src/com/cn/zhaohjg/git/test/HelloWorld.java
@@ -1,5 +1,11 @@
 package com.cn.zhaohjg.git.test;
 
+/**
+ * http://hentai-wallpapers.com/Hentai-categorized-albums/Hentai-color-comics-34c.html
+ * http://freedreamgirls.com/
+ *
+ */
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
