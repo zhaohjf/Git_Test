cb50a6cc9d118b3ae2f1cc35dd4a7a5d23e7e8c4
zhaohjf <zhaohj0315@163.com>
2015-12-01 20:25:54 +0800

    zhaohjf_20151201晚
    Change-Id: If33cead80241230acf2d772adb1aef5bf7de0b8e
    Signed-off-by: zhaohjf <zhaohj0315@163.com>

--- src/com/cn/zhaohjg/git/test/HelloWorld.java
+++ src/com/cn/zhaohjg/git/test/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.cn.zhaohjg.git.test;
 
 /**
+ * http://download.eclipse.org/egit/updates/ Git°²×°µØÖ·
+ * 
  * http://hentai-wallpapers.com/Hentai-categorized-albums/Hentai-color-comics-34c.html
  * http://freedreamgirls.com/
  *
